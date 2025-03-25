635a5cc6a26d58674f232a7151f2cf9dca852a13
AlbarDigidaw <chiyokoarata@gmail.com>
2025-03-25 21:58:42 +0700

    Add files via upload

---
+++ contact.blade.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+
+  <title>IKA</title>
+
+   <!-- slider stylesheet -->
+   <link rel="stylesheet" type="text/css"
+    href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.3/assets/owl.carousel.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="templet/css/bootstrap.css" />
+
+  <!-- fonts style -->
+  <link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet" />
+  <!-- Custom styles for this template -->
+  <link href="templet/css/style.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="templet/css/responsive.css" rel="stylesheet" />
+
+<!-- slider stylesheet -->
+
+
+<!-- bootstrap core css -->
+
+
+<!-- Custom styles for this template -->
+<link href="templet2/css/style2.css" rel="stylesheet" />
+<!-- responsive style -->
+<link href="templet2/css/responsive.css" rel="stylesheet" />
+</head>
+
+<body>
+  <div class="hero_area sub_pages">
+    <!-- header section strats -->
+    <header class="header_section">
+      <div class="container">
+        <nav class="navbar navbar-expand-lg custom_nav-container pt-3">
+          <a class="navbar-brand" href="index.html">
+            <img src="templet/images/IKA2.png" alt="" /><span>
+            <h2 class="ika">IKA</h2>
+            </span>
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="d-flex ml-auto flex-column flex-lg-row align-items-center">
+              <ul class="navbar-nav  ">
+                <li class="nav-item active">
+                  <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="about"> About </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="contact">Contact us</a>
+                </li>
+              </ul>
+            </div>
+            <div class="quote_btn-container ml-0 ml-lg-4 d-flex justify-content-center">
+              <a href="">
+                Cetak Kartu Peserta
+              </a>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <!-- end header section -->
+  </div>
+
+
+
+  <!-- contact section -->
+  <section class="contact_section layout_padding">
+    <div class="container">
+      <h2 class="font-weight-bold">
+        Contact Us
+      </h2>
+      <div class="row">
+        <div class="col-md-8 mr-auto">
+          <form action="">
+            <div class="contact_form-container">
+              <div>
+                <div>
+                  <input type="text" placeholder="Name">
+                </div>
+                <div>
+                  <input type="text" placeholder="Phone Number">
+                </div>
+                <div>
+                  <input type="email" placeholder="Email">
+                </div>
+
+                <div class="mt-5">
+                  <input type="text" placeholder="Message">
+                </div>
+                <div class="mt-5">
+                  <button type="submit">
+                    send
+                  </button>
+                </div>
+              </div>
+
+            </div>
+
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- end contact section -->
+
+
+<!-- info section -->
+<br>
+<br>
+  <section class="info_section  layout_padding2-top">
+    <div class="social_container">
+        <div class="social_box">
+            <a href="https://www.facebook.com/">
+                <i class="fa fa-facebook" aria-hidden="true"></i>
+            </a>
+        <a href="https://www.instagram.com/ika_yumaga/">
+          <i class="fa fa-instagram" aria-hidden="true"></i>
+        </a>
+    </div>
+</div>
+<div class="info_container ">
+    <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-lg-3">
+              <h6>
+                  ABOUT US
+                </h6>
+                <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doLorem ipsum dolor sit amet, consectetur adipiscing elit, sed doLorem ipsum dolor sit amet,
+            </p>
+        </div>
+          <div class="col-md-6 col-lg-1">
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <h6>
+                NEED HELP
+            </h6>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doLorem ipsum dolor sit amet, consectetur adipiscing elit, sed doLorem ipsum dolor sit amet,
+            </p>
+        </div>
+        <div class="col-md-6 col-lg-1">
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <h6>
+                CONTACT US
+            </h6>
+            <div class="info_link-box">
+              <a href="">
+                  <i class="fa fa-map-marker" aria-hidden="true"></i>
+                <span> Gb road 123 london Uk </span>
+              </a>
+              <a href="">
+                  <i class="fa fa-phone" aria-hidden="true"></i>
+                  <span>+01 12345678901</span>
+              </a>
+              <a href="">
+                <i class="fa fa-envelope" aria-hidden="true"></i>
+                <span> demo@gmail.com</span>
+            </a>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+<!-- info end section -->
+
+</section>
+<br>
+<!-- footer section -->
+    <section class="container-fluid footer_section">
+    <p>
+      Copyright &copy; 2025 All Rights Reserved By
+      <a href="https://www.instagram.com/ika_yumaga/">IKA YUMAGA</a>
+    </p>
+    <!-- footer section -->
+
+    <script type="text/javascript" src="templet/js/jquery-3.4.1.min.js"></script>
+  <script type="text/javascript" src="templet/js/bootstrap.js"></script>
+  <script src="templet2/js/jquery-3.4.1.min.js"></script>
+  <script src="temple2t/js/bootstrap.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
+  </script>
+  <script src="templet2/js/custom.js"></script>
+  <!-- google map js -->
+</body>
+
+</html>
